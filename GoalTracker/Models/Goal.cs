c39b2a9e37c6fbeb4f5dde00e5ffaf05a082d165
Tichin Wu <twu38@mail.ccsf.edu>
2025-09-22 22:41:48 -0700

    Add Goal object

--- GoalTracker/Models/Goal.cs
+++ GoalTracker/Models/Goal.cs
@@ -2,12 +2,24 @@ namespace GoalTracker.Models
 {
     public class Goal
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime TargetDate { get; set; }
         public double Progress { get; set; }   // 0.0 – 1.0
-        public string Motivation { get; set; }
-        public string Details { get; set; } //for now just a string, could be expanded later
+        public required string Motivation { get; set; }
+        public Goal() // new parameterless ctor
+        {
+            StartDate = DateTime.Now;
+            TargetDate = StartDate.AddDays(30);
+        }
+
+        public Goal(string name, string motivation)
+        {
+            Name = name;
+            Motivation = motivation;
+            StartDate = DateTime.Now;
+            TargetDate = StartDate.AddDays(30);
+        }
 
         public string ProgressPercent => $"{(int)(Progress * 100)}%";
     }
